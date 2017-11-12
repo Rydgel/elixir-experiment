fb8f6b62e8e465ef65790f88edae4c5fd68e7c5a
Jérôme Mahuet <jerome.mahuet@gmail.com>
2017-11-12 02:07:13 +0100

    server process

---
+++ lib/servy/pledge_server.ex
@@ -0,0 +1,70 @@
+defmodule Servy.PledgeServer do
+  @moduledoc """
+  """
+
+  @process_name :pledge_server
+
+  def start(initial_state \\ []) do
+    IO.puts "Starting the pledge server..."
+    pid = spawn(__MODULE__, :listen_loop, [initial_state])
+    Process.register(pid, @process_name)
+    pid
+  end
+
+  def create_pledge(name, amount) do
+    send @process_name, {self(), :create_pledge, name, amount}
+    receive do {:response, status} -> status end
+  end
+
+  def recent_pledges() do
+    send @process_name, {self(), :recent_pledges}
+    receive do {:response, pledges} -> pledges end
+  end
+
+  def total_pledges() do
+    send @process_name, {self(), :total_pledges}
+    receive do {:response, total} -> total end
+  end
+
+  def listen_loop(state) do
+    receive do
+      {sender, :create_pledge, name, amount} ->
+        {:ok, id} = send_pledge_to_service(name, amount)
+        most_recent_pledges = Enum.take(state, 2)
+        new_state = [{name, amount} | most_recent_pledges]
+        send sender, {:response, id}
+        listen_loop(new_state)
+      {sender, :recent_pledges} ->
+        send sender, {:response, state}
+        listen_loop(state)
+      {sender, :total_pledges} ->
+        total = Enum.map(state, &elem(&1, 1)) |> Enum.sum
+        send sender, {:response, total}
+        listen_loop(state)
+      unexpected ->
+        IO.puts "Unexpected message: #{inspect unexpected}"
+        listen_loop(state)
+    end
+  end
+
+  defp send_pledge_to_service(_name, _amount) do
+    # CODE to send pledge to external service
+    {:ok, "pledge-#{:rand.uniform(1000)}"}
+  end
+end
+
+alias Servy.PledgeServer
+
+pid = PledgeServer.start()
+
+IO.inspect PledgeServer.create_pledge("larry", 10)
+IO.inspect PledgeServer.create_pledge("moe", 20)
+IO.inspect PledgeServer.create_pledge("curly", 30)
+IO.inspect PledgeServer.create_pledge("daisy", 40)
+IO.inspect PledgeServer.create_pledge("grace", 50)
+
+IO.inspect PledgeServer.recent_pledges()
+
+IO.inspect PledgeServer.total_pledges()
+
+IO.inspect Process.info(pid, :messages)
